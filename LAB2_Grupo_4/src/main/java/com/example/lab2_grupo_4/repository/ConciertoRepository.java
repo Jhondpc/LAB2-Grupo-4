1f6579c8e7c7b4b0768a68c4759d84330e1d8763
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-05 08:16:26 -0500

    borrado

--- LAB2_Grupo_4/src/main/java/com/example/lab2_grupo_4/repository/ConciertoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab2_grupo_4.repository;
-
-import com.example.lab2_grupo_4.entity.Concierto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConciertoRepository extends JpaRepository<Concierto,Integer> {
-}
