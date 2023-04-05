002277b3826666961411ac85a1ca32b3bc9d98f7
Jhondpc <a20196137@pucp.edu.pe>
2023-04-04 21:34:25 -0500

    entity creado

--- LAB2_Grupo_4/src/main/java/com/example/lab2_grupo_4/repository/ArtistasRepository.java
+++ LAB2_Grupo_4/src/main/java/com/example/lab2_grupo_4/repository/ArtistasRepository.java
@@ -1,5 +1,6 @@
 package com.example.lab2_grupo_4.repository;
 
+import com.example.lab2_grupo_4.entity.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
