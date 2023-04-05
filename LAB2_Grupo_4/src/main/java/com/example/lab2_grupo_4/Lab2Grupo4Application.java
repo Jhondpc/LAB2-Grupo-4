11ec9a433596679e21b927682ea7655c9da0b099
Jhondpc <a20196137@pucp.edu.pe>
2023-04-04 20:39:54 -0500

    proyecto creado

---
+++ LAB2_Grupo_4/src/main/java/com/example/lab2_grupo_4/Lab2Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_grupo_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Grupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Grupo4Application.class, args);
+	}
+
+}
